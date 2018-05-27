718a122a484386a477e4a1d56077c806af3c5612
Hilda Ng <nghilda90@gmail.com>
2018-05-27 15:51:51 +0800

    Final solution of mummify method

--- string_mummifier.py
+++ string_mummifier.py
@@ -1,3 +1,27 @@
 class StringMummifier:
+    VOWELS = 'aeiou'
+    REPLACEMENT = 'mummy'
+
     def mummify(self, word):
-        return ''
+        if self.__is_vowel_count_less_than_30_percent(word):
+            return word
+        return self.__replace_vowels(word)
+
+    def __is_a_vowel(self, char):
+        return char.lower() in StringMummifier.VOWELS
+
+    def __is_vowel_count_less_than_30_percent(self, word):
+        vowel_count = 0
+        for char in word:
+            if self.__is_a_vowel(char):
+                vowel_count += 1
+        return len(word) == 0 or vowel_count / len(word) < 0.3
+
+    def __replace_vowels(self, word):
+        mummified = ""
+        for char in word:
+            if not self.__is_a_vowel(char):
+                mummified += char
+            elif not mummified.endswith(StringMummifier.REPLACEMENT):
+                mummified += StringMummifier.REPLACEMENT
+        return mummified
--- test_string_mummifier.py
+++ test_string_mummifier.py
@@ -3,11 +3,38 @@ from unittest import TestCase
 from string_mummifier import StringMummifier
 
 
-class TestStringMummifier(TestCase):
-    def test_should_not_mummify_empty_string(self):
-        word = ""
+class StringMummifierTestCase(TestCase):
 
+    def test_should_not_mummify_empty_string(self):
+        self.assert_correct_mummification("", "")
+
+    def test_should_not_mummify_words_with_no_vowels(self):
+        self.assert_correct_mummification("str", "str")
+
+    def test_should_mummify_a_vowel(self):
+        self.assert_correct_mummification("mummy", "a")
+
+    def test_should_mummify_consonents_and_a_vowel(self):
+        self.assert_correct_mummification("blmummy", "bla")
+
+    def test_should_not_mummify_less_than_30_percent_vowels(self):
+        self.assert_correct_mummification("blah", "blah")
+
+    def test_should_mummify_continuous_vowels_once(self):
+        self.assert_correct_mummification("hmummyr", "hear")
+
+    def test_should_mummify_multiple_sets_of_vowels(self):
+        self.assert_correct_mummification("blmummyhmummyh", "blaahah")
+
+    def test_should_mummify_capital_vowels(self):
+        self.assert_correct_mummification("blmummyhmummy", "blAhE")
+
+    def test_should_error_on_none_input(self):
+        with self.assertRaises(Exception):
+            StringMummifier().mummify(None)
+
+    def assert_correct_mummification(self, expected, word):
         mummifier = StringMummifier()
         mummified_word = mummifier.mummify(word)
+        self.assertEqual(expected, mummified_word)
 
-        self.assertEqual("", mummified_word)
